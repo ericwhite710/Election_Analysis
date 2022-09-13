34b37b7bc5b764f9bccbadc556b734d4c59c5011
Eric White <110315163+ericwhite710@users.noreply.github.com>
2022-09-13 15:35:46 -0400

    Added Updated PyPoll.py
    Updated vote count to candidates

--- PyPoll.py
+++ PyPoll.py
@@ -30,29 +30,51 @@
 
 #add our dependencies
 import csv
-from email import header
 import os
 
 #assign a variable for the file to load and thee path
-#with open("election_results.csv", "r") as election_data:
 file_to_load = os.path.join("Resources", "election_results.csv")
 
-#create a filename variable to a direct or indirect path to the file
+#assign a variable for the file to save to the path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+#1. Initialize a total vote counter
+total_votes = 0
+#candidate options
+candidate_options = []
+#candidate votes dictionary
+candidate_votes = {}
+
 
 #open the election results and read the file
 with open(file_to_load) as election_data:
-
-#to do: read and analyze the data here
     file_reader = csv.reader(election_data)
 
-#print each row in the csv file
-#    for row in file_reader:
-#        print (row)
-
-#print the header row
+    #read the header row
     headers = next(file_reader)
-    print(headers)
+
+    #print each row in the csv file
+    for row in file_reader:
+        #2. Add to the total vote count
+        total_votes += 1
+
+        #print the candidate name from each row
+        candidate_name = row[2]
+
+        
+        #add the candidate name to the candidate list
+        #if the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            #add it to the list of candidates
+            candidate_options.append(candidate_name)
+            #2. track candidate vote count
+            candidate_votes[candidate_name] = 0
+        #add a vote to candidate count
+        candidate_votes[candidate_name] += 1
+
+#print the candidate vote dictionary
+print (candidate_votes)
+
 
 
 
